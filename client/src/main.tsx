9e56cb20f439eb08a27ff31fe43b1e1252adc581
giselegal <consultoria@giselegalvao.com.br>
2025-07-03 05:00:43 +0000

    atualizar

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,16 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-<<<<<<< HEAD
-=======
-import {
-  initializeResourcePreloading,
-  setupRouteChangePreloading,
-} from "./utils/preloadResources";
-import { fixMainRoutes } from "./utils/fixMainRoutes";
-import { checkMainRoutes } from "./utils/routeChecker";
-// import "./utils/hotmartWebhookSimulator"; // Carregar simulador de webhook - temporariamente desabilitado
->>>>>>> e3df6263084c251ab32a2aa425fbb30b0a930527
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
