0aca7e5c37b8357e77d7fd678c36a782135488e9
giselegal <consultoria@giselegalvao.com.br>
2025-07-06 23:34:07 +0000

    quis publicado

--- client/src/components/editor/blocks/index.ts
+++ client/src/components/editor/blocks/index.ts
@@ -1,49 +1,13 @@
+// Componentes básicos confirmados como existentes
 export { default as HeaderBlock } from './HeaderBlock';
 export { default as TextBlock } from './TextBlock';
 export { ImageBlock } from './ImageBlock';
 export { default as ButtonBlock } from './ButtonBlock';
-export { SpacerBlock } from './SpacerBlock';
-export { default as ResultHeaderBlock } from './ResultHeaderBlock';
-export { default as ResultDescriptionBlock } from './ResultDescriptionBlock';
-export { default as ProductOfferBlock } from './ProductOfferBlock';
-export { default as UrgencyTimerBlock } from './UrgencyTimerBlock';
-export { default as FAQSectionBlock } from './FAQSectionBlock';
-export { default as TestimonialsBlock } from './TestimonialsBlock';
-export { GuaranteeBlock } from './GuaranteeBlock';
-export { VideoPlayerBlock } from './VideoPlayerBlock';
-export { InlineEditableText } from './InlineEditableText';
+export { default as SpacerBlock } from './SpacerBlock';
 
-// Novos blocos UI/Avançados - usando export default
-export { default as AlertBlock } from './AlertBlock';
-export { default as ArgumentsBlock } from './ArgumentsBlock';
-export { default as AudioBlock } from './AudioBlock';
-export { default as CarouselBlock } from './CarouselBlock';
-export { default as LoaderBlock } from './LoaderBlock';
-export { default as CompareBlock } from './CompareBlock';
-export { default as ConfettiBlock } from './ConfettiBlock';
-export { default as QuoteBlock } from './QuoteBlock';
-export { default as FormInputBlock } from './FormInputBlock';
-export { default as ChartAreaBlock } from './ChartAreaBlock';
-export { default as ChartLevelBlock } from './ChartLevelBlock';
-export { default as ListBlock } from './ListBlock';
-export { default as MarqueeBlock } from './MarqueeBlock';
-export { default as OptionsGridBlock } from './OptionsGridBlock';
-export { default as ScriptBlock } from './ScriptBlock';
-export { default as TermsBlock } from './TermsBlock';
-
-// Blocos especiais das etapas do funil
-export { default as QuizStartPageBlock } from './QuizStartPageBlock';
-export { default as ResultPageBlock } from './ResultPageBlock';
-export { default as QuizOfferPageBlock } from './QuizOfferPageBlock';
-
-// Quiz-specific blocks
-export { default as QuizIntroBlock } from '../../blocks/quiz/QuizIntroBlock';
-export { default as QuizQuestionBlock } from './QuizQuestionBlock';
-export { default as StrategicQuestionBlock } from './StrategicQuestionBlock';
-export { default as QuizTransitionBlock } from './QuizTransitionBlock';
-export { default as QuestionMultipleBlock } from './QuestionMultipleBlock';
-
-// Novos blocos avançados baseados no modelo
+// Componentes avançados modernos
 export { default as RichTextBlock } from './RichTextBlock';
 export { default as QuizStepBlock } from './QuizStepBlock';
 
+// Utilitário para edição inline
+export { InlineEditableText } from './InlineEditableText';
