bdba4f56c2620773a018a2899f51313601bcfbc0
giselegal <consultoria@giselegalvao.com.br>
2025-07-06 03:24:34 +0000

    quis publicado

--- client/src/pages/CaktoQuizAdvancedPage.tsx
+++ client/src/pages/CaktoQuizAdvancedPage.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import CaktoQuizAdvancedEditor from '@/components/visual-editor/CaktoQuizAdvancedEditorFixed';
+import SchemaDrivenEditorLayoutV2 from '@/components/editor/SchemaDrivenEditorLayoutV2';
 
 const CaktoQuizAdvancedPage: React.FC = () => {
   return (
-    <div className="min-h-screen bg-background">
-      <CaktoQuizAdvancedEditor />
+    <div className="min-h-screen bg-gray-100">
+      <SchemaDrivenEditorLayoutV2 />
     </div>
   );
 };
