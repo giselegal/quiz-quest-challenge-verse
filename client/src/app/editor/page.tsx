8f625546ca12a0c4ca00c861bcbab0267b8d2bdc
giselegal <consultoria@giselegalvao.com.br>
2025-07-08 22:12:48 +0000

    editor

---
+++ client/src/app/editor/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react';
+import QuizEditorInterface from '@/components/editor/QuizEditorInterface';
+
+export default function EditorPage() {
+  return (
+    <div className="min-h-screen bg-background">
+      <QuizEditorInterface />
+    </div>
+  );
+}
