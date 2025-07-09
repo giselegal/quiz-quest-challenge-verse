f59b741a7abb8eda7fc730945bb749ab89abb4eb
giselegal <consultoria@giselegalvao.com.br>
2025-07-09 16:18:02 +0000

    Descrição das suas alterações

--- client/src/pages/CaktoQuizAdvancedPage.tsx
+++ client/src/pages/CaktoQuizAdvancedPage.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import SchemaDrivenEditorLayoutV2 from '@/components/editor/SchemaDrivenEditorLayoutV2';
+import SchemaDrivenEditorResponsive from '@/components/editor/SchemaDrivenEditorResponsive';
 
 const CaktoQuizAdvancedPage: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-100">
-      <SchemaDrivenEditorLayoutV2 />
+      <SchemaDrivenEditorResponsive />
     </div>
   );
 };
