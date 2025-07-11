202291299a702c3ef72e0dff9f15c32c7f0f42ec
giselegal <consultoria@giselegalvao.com.br>
2025-07-11 10:37:57 +0000

    Descrição das suas alterações

--- client/src/components/editor/index.ts
+++ client/src/components/editor/index.ts
@@ -3,25 +3,30 @@
  * Exportações centralizadas dos componentes do editor
  */
 
-// Principais componentes do editor
+// Componentes principais do editor (export default)
 export { default as ComponentList } from './ComponentList';
 export { default as PageEditorCanvas } from './PageEditorCanvas';
 export { default as ModernQuizEditor } from './ModernQuizEditor';
 export { default as ModularQuizEditor } from './ModularQuizEditor';
 export { default as EditorLayout } from './EditorLayout';
+export { default as SalesPageEditor } from './SalesPageEditor';
+export { default as QuizEditorInterface } from './QuizEditorInterface';
 export { default as QuizEditorSteps } from './QuizEditorSteps';
 
-// Componentes com named exports
-export { PropertyPanel } from './PropertyPanel';
-export { ModernPropertyPanel } from './ModernPropertyPanel';
+// Componentes de UI e blocos (export named)
+export { AddBlockButton } from './AddBlockButton';
 export { BlockRenderer } from './BlockRenderer';
 export { EditorBlockItem } from './EditorBlockItem';
-export { AddBlockButton } from './AddBlockButton';
-export { EmptyEditor } from './EmptyEditor';
 export { EditBlockContent } from './EditBlockContent';
-export { PageEditor } from './PageEditor';
-export { UnifiedEditorLayout } from './UnifiedEditorLayout';
+export { EmptyEditor } from './EmptyEditor';
 
-// Testes e outros
+// Painéis de propriedades (export named)
+export { PropertyPanel } from './PropertyPanel';
+export { ModernPropertyPanel } from './ModernPropertyPanel';
+
+// Layouts responsivos (export default)
+export { default as SchemaDrivenEditorResponsive } from './SchemaDrivenEditorResponsive';
+
+// Testes e exemplos (export default)
 export { default as EditorTestPage } from './EditorTestPage';
 export { default as SimpleEditorTest } from './SimpleEditorTest';
